fd1efd6bf5d1c786f58e1ab95fa6cb3b1ba6972a
Shreyas Ambre <shreyas.ambre@consultant.mannai.com.qa>
2026-03-25 20:22:34 +0530

    previous changes revert

--- src/App.jsx
+++ src/App.jsx
@@ -8,57 +8,57 @@ import AllProducts from './components/AllProducts/AllProducts';
 import Layout from './components/Layout/Layout';
 import Contact from './components/Contact/Contact';
 // Server Side Routing
-// export default function App() {
-//   const router = createBrowserRouter([
-//     { 
-//       path: '/', 
-//       element: <Layout />, 
-//       children: [
-//         {
-//           path: '/',
-//           element: <Home />
-//         },
-//         {
-//           path: '/fruits',
-//           element: <Fruits />
-//         },
-//         {
-//           path: '/dairy',
-//           element: <Dairy />
-//         },
-//         {
-//           path: '/seafood',
-//           element: <SeeFood />
-//         },
-//         {
-//           path: '/all-products',
-//           element: <AllProducts />
-//         },
-//         {
-//           path: '/contact',
-//           element: <Contact />
-//         }
-//       ]
-//     }, 
+export default function App() {
+  const router = createBrowserRouter([
+    { 
+      path: '/', 
+      element: <Layout />, 
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/fruits',
+          element: <Fruits />
+        },
+        {
+          path: '/dairy',
+          element: <Dairy />
+        },
+        {
+          path: '/seafood',
+          element: <SeeFood />
+        },
+        {
+          path: '/all-products',
+          element: <AllProducts />
+        },
+        {
+          path: '/contact',
+          element: <Contact />
+        }
+      ]
+    }, 
     
-//   ])
-//   return <RouterProvider router={router} />
-// }
+  ])
+  return <RouterProvider router={router} />
+}
 
 // Static Routing
-export default function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
-          <Route path="fruits" element={<Fruits />} />
-          <Route path="dairy" element={<Dairy />} />
-          <Route path="seafood" element={<SeeFood />} />
-          <Route path="all-products" element={<AllProducts />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </HashRouter>
-  )
-}
+// export default function App() {
+//   return (
+//     <HashRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />} >
+//           <Route index element={<Home />} />
+//           <Route path="fruits" element={<Fruits />} />
+//           <Route path="dairy" element={<Dairy />} />
+//           <Route path="seafood" element={<SeeFood />} />
+//           <Route path="all-products" element={<AllProducts />} />
+//           <Route path="contact" element={<Contact />} />
+//         </Route>
+//       </Routes>
+//     </HashRouter>
+//   )
+// }
