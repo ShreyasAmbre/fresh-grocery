93ef45789718147f4e372aabaff8ebdc71e71920
Shreyas Ambre <shreyas.ambre@consultant.mannai.com.qa>
2026-03-25 20:13:16 +0530

    route change from server side routing to statis routing for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,64 @@
 import React from 'react'
 import Home from './components/Home/Home';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, HashRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import Fruits from './components/Fruits/Fruits';
 import Dairy from './components/Dairy/Dairy';
 import SeeFood from './components/SeeFood/SeeFood';
 import AllProducts from './components/AllProducts/AllProducts';
 import Layout from './components/Layout/Layout';
 import Contact from './components/Contact/Contact';
-
-export default function App() {
-  const router = createBrowserRouter([
-    { 
-      path: '/', 
-      element: <Layout />, 
-      children: [
-        {
-          path: '/',
-          element: <Home />
-        },
-        {
-          path: '/fruits',
-          element: <Fruits />
-        },
-        {
-          path: '/dairy',
-          element: <Dairy />
-        },
-        {
-          path: '/seafood',
-          element: <SeeFood />
-        },
-        {
-          path: '/all-products',
-          element: <AllProducts />
-        },
-        {
-          path: '/contact',
-          element: <Contact />
-        }
-      ]
-    }, 
+// Server Side Routing
+// export default function App() {
+//   const router = createBrowserRouter([
+//     { 
+//       path: '/', 
+//       element: <Layout />, 
+//       children: [
+//         {
+//           path: '/',
+//           element: <Home />
+//         },
+//         {
+//           path: '/fruits',
+//           element: <Fruits />
+//         },
+//         {
+//           path: '/dairy',
+//           element: <Dairy />
+//         },
+//         {
+//           path: '/seafood',
+//           element: <SeeFood />
+//         },
+//         {
+//           path: '/all-products',
+//           element: <AllProducts />
+//         },
+//         {
+//           path: '/contact',
+//           element: <Contact />
+//         }
+//       ]
+//     }, 
     
-  ])
-  return <RouterProvider router={router} />
+//   ])
+//   return <RouterProvider router={router} />
+// }
+
+// Static Routing
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="fruits" element={<Fruits />} />
+          <Route path="dairy" element={<Dairy />} />
+          <Route path="seafood" element={<SeeFood />} />
+          <Route path="all-products" element={<AllProducts />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  )
 }
